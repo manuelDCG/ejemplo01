5cf7523ba2f0966c6c87f4797d8558cbfacbe118
IBT <IBT@DESKTOP-2VCI08U>
2020-11-09 23:15:53 -0400

    se agregó "principal.cpp"

---
+++ Project1/Project1/principal.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "nombre completo : Manuel Dominique Cruz Grimaldez" << endl;
+	cout << "CU : 35-4879" << endl;
+	cout << "CI : 13188455" << endl;
+	cout << "edad : 20 años" << endl;
+}
